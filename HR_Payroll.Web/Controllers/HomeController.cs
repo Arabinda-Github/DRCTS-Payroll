6d9147a7d7a1495e1c9ebbaa52092503814e87a3
Arabinda-Github <arabindamaharana744@gmail.com>
2025-10-08 10:59:10 +0530

    init new proj

---
+++ HR_Payroll.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using HR_Payroll.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HR_Payroll.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
